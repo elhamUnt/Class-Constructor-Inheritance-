20a6c25b77063aa947dbfe43b49e0a003e86fabe
elhamUnt <elham.hamiti@unt.edu.mk>
2026-05-26 16:25:15 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Klasa bazë
+class Personi {
+protected:
+    string emri;
+    int mosha;
+
+public:
+    // Konstruktor
+    Personi(string e, int m) {
+        emri = e;
+        mosha = m;
+        cout << "Konstruktori i Personit u thirr" << endl;
+    }
+
+    void shfaqInfo() {
+        cout << "Emri: " << emri << endl;
+        cout << "Mosha: " << mosha << endl;
+    }
+};
+
+// Klasa e trasheguar
+class Studenti : public Personi {
+private:
+    int nota;
+
+public:
+    // Konstruktor i Studentit
+    Studenti(string e, int m, int n)
+        : Personi(e, m)   // thërret konstruktorin e Personit
+    {
+        nota = n;
+        cout << "Konstruktori i Studentit u thirr" << endl;
+    }
+
+    void shfaqStudent() {
+        shfaqInfo();
+        cout << "Nota: " << nota << endl;
+    }
+};
+
+int main() {
+    Studenti s("Ardit", 20, 10);
+
+    cout << endl;
+
+    s.shfaqStudent();
+
+    return 0;
+}
